45a00cd35c953a37a02756f497c375779445ecf2
Ignat <bagaev.ignat@gmail.com>
2016-04-27 01:19:06 +0300

    make user to be able to create questions

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns new question to @question' do
+      expect(assigns(:question)).to be_a_new Question
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+    before { get :show, id: question}
+    
+    it 'assigns the reqested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+    
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves new questioh to database' do
+        expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by 1
+      end
+      it 'redirects to show view' do
+        post :create, question: attributes_for(:question)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid attributes' do 
+      it 'does not save question to database' do
+        expect { post :create, question: attributes_for(:invalid_question) }.to_not change(Question, :count)
+      end
+      it 'renders new view' do
+        post :create, question: attributes_for(:invalid_question)
+        expect(response).to render_template :new
+      end
+    end
+  end
+end

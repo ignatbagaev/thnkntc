c01bb143475f53d21e42b7ba001db39177c026b7
Ignat <bagaev.ignat@gmail.com>
2016-05-01 18:41:03 +0300

    fix the way of getting answers on question page

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,2 +1,5 @@
 module AnswersHelper
+  def answers_of(question_id)
+    Answer.where(question_id: question_id)
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,8 +3,4 @@ class Question < ActiveRecord::Base
   belongs_to :user
   
   validates :title, :body, :user_id, presence: true
-
-  def answers
-    Answer.where(question_id: self.id)
-  end
 end

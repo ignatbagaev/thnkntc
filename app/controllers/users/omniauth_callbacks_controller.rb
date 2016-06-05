52060ea0781ca6319ba0c0cb6a1b09f085c112a0
Ignat <bagaev.ignat@gmail.com>
2016-06-05 14:54:49 +0300

    fix oauth callback and wrote spec to test it's behaviour

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,19 +1,26 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  before_action :oauth_callback
-
-  def facebook; end
-  def twitter; end
+  def facebook
+    oauth_callback
+  end
+  
+  def twitter
+    oauth_callback
+  end
 
   private
 
   def oauth_callback
-    return unless request.env['omniauth.auth']
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: action_name.capitalize) if is_navigational_format?
+    if request.env['omniauth.auth']
+      @user = User.find_for_oauth(request.env['omniauth.auth'])
+      if @user
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message(:notice, :success, kind: action_name.capitalize) if is_navigational_format?
+      else
+        session['devise.omniauth'] = request.env['omniauth.auth'].slice(:provider, :uid)
+        redirect_to new_user_registration_path
+      end
     else
-      session['devise.omniauth'] = request.env['omniauth.auth'].slice(:provider, :uid)
+      set_flash_message :alert, :failure, kind: action_name.capitalize, reason: failure_message
       redirect_to new_user_registration_path
     end
   end
--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,9 +1,111 @@
 require 'rails_helper'
 
 RSpec.describe Users::OmniauthCallbacksController, type: :controller do
+  before { @request.env["devise.mapping"] = Devise.mappings[:user] }
+
   describe 'GET #facebook' do
+
+    context 'when no oauth_data' do
+      before do
+        get :facebook
+      end
+      it 'should redirect_to new user registration' do
+        expect(response).to redirect_to new_user_registration_path
+      end
+    end
+
+    context 'when user exists' do  
+      let(:user) { create :user }
+      
+      before do
+        stub_env_for_omniauth(info: { email: user.email })
+        get :facebook
+      end
+      it { should be_user_signed_in }
+      it { should redirect_to root_path }
+    end
+
+    context 'when user not exists' do
+
+      context 'email is provided' do
+        before do
+          stub_env_for_omniauth
+          get :facebook
+        end
+        it { should be_user_signed_in }
+        it { should redirect_to root_path }
+      end
+
+      context 'email is not provided' do
+        before do
+          stub_env_for_omniauth(info: nil)
+          get :facebook
+        end
+
+        it 'stores data in session' do
+          hash = stub_env_for_omniauth
+          expect(session["devise.omniauth"][:provider]).to eq hash.provider
+          expect(session["devise.omniauth"][:uid]).to eq hash.uid
+        end
+
+        it { should_not be_user_signed_in }
+        it { should redirect_to new_user_registration_path }
+      end
+    end    
   end
 
   describe 'GET #twitter' do
+    context 'when no oauth_data' do
+      before do
+        get :twitter
+      end
+      it 'should redirect_to new user registration' do
+        expect(response).to redirect_to new_user_registration_path
+      end
+    end
+
+    context 'when user exists' do  
+      let(:user) { create :user }
+      
+      before do
+        stub_env_for_omniauth(provier: 'twitter', info: { email: user.email })
+        get :twitter
+      end
+      it { should be_user_signed_in }
+      it { should redirect_to root_path }
+    end
+
+    context 'when user not exists' do
+
+      context 'when email is provided' do
+        before do
+          stub_env_for_omniauth(provier: 'twitter')
+          get :twitter
+        end
+        it { should be_user_signed_in }
+        it { should redirect_to root_path }
+      end
+
+      context 'when email is not provided' do
+        before do
+          stub_env_for_omniauth(provier: 'twitter', info: nil)
+          get :twitter
+        end
+
+        it 'stores data in session' do
+          hash = stub_env_for_omniauth(provier: 'twitter')
+          expect(session["devise.omniauth"][:provider]).to eq hash.provider
+          expect(session["devise.omniauth"][:uid]).to eq hash.uid
+        end
+
+        it { should_not be_user_signed_in }
+        it { should redirect_to new_user_registration_path }
+      end
+    end    
+  end
+
+  def stub_env_for_omniauth(hash = {})
+    hash = OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: 'user@test.com' }).merge(hash)
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[hash.provider.to_sym] = hash
   end
 end

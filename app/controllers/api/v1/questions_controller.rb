fb199f3c3050d271692b409fcb2ac37a942d8f22
Ignat <bagaev.ignat@gmail.com>
2016-06-15 20:55:23 +0300

    returning requested question for API
    Implemented action :show in API::V1::QuestionsController to return requested question, it's comments and urls for it's attachments

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -11,6 +11,11 @@ module Api
         @questions = Question.all
         respond_with @questions
       end
+
+      def show
+        @question = Question.find(params[:id])
+        respond_with @question
+      end
     end
   end
 end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -38,4 +38,65 @@ describe 'Questions API' do
       end
     end
   end
+
+  describe 'GET #show' do
+    let(:question) { create :question }
+
+    context 'when user is not authenticated' do
+      it 'return status 401 if there is no access token' do
+        get "/api/v1/questions/#{question.id}", format: :json
+        expect(response.status).to eq 401
+      end
+
+      it 'return status 401 if there is invalid access token' do
+        get "/api/v1/questions/#{question.id}", format: :json, access_token: '12345'
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'when user authenticated' do
+      let(:access_token) { create :access_token }
+      let(:comments) { create_list(:comment, 2) }
+      let(:attachments) { create_list(:attachment, 2) }
+
+      before do
+        question.comments << comments
+        question.attachments << attachments
+        get "/api/v1/questions/#{question.id}", format: :json, access_token: access_token.token
+      end
+
+      it 'returns status 200' do
+        expect(response.status).to eq 200
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "contains #{attr} in question" do
+          expect(response.body)
+            .to be_json_eql(question.send(attr.to_sym).to_json)
+            .at_path("question/#{attr}")
+        end
+      end
+
+      context 'comments' do
+        %w(id body created_at updated_at).each do |attr|
+          it "contains #{attr} in comment" do
+            comment = comments.last
+            expect(response.body)
+              .to be_json_eql(comment.send(attr.to_sym).to_json)
+              .at_path("question/comments/0/#{attr}")
+          end
+        end
+      end
+
+      context 'attachments' do
+        it "contains url for each question attachment" do
+          attachment = attachments.last
+          puts response.body
+          expect(response.body)
+            .to be_json_eql(attachment.file.url.to_json)
+            .at_path("question/attachments/0/url")
+        end
+      end
+    end
+  end
 end

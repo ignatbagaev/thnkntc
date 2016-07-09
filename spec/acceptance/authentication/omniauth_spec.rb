d1cabde36fa814a0044976172a5431e549c4cd1a
Ignat <bagaev.ignat@gmail.com>
2016-07-09 23:17:55 +0300

    fix omniauth specs

--- spec/acceptance/authentication/omniauth_spec.rb
+++ spec/acceptance/authentication/omniauth_spec.rb
@@ -1,26 +1,28 @@
 require_relative '../acceptance_helper'
 
 feature 'through facebook' do
+
   scenario 'can sign in user with facebook account' do
     visit new_user_session_path
     mock_facebook_auth_hash
-    click_link 'Sign in with Facebook'
+    page.find(:xpath, "//a[contains(@href,'auth/facebook')]").click
     expect(page).to have_content('Log out')
   end
 
   scenario 'can handle authentication error' do
     OmniAuth.config.mock_auth[:facebook] = :invalid_credentials
     visit new_user_session_path
-    click_link 'Sign in with Facebook'
+    page.find(:xpath, "//a[contains(@href,'auth/facebook')]").click
     expect(page).to have_content('Could not authenticate you from Facebook')
   end
 end
 
 feature 'through twitter' do
+  
   scenario 'can sign UP user with twitter account' do
     visit new_user_session_path
     mock_twitter_auth_hash
-    click_link 'Sign in with Twitter'
+    page.find(:xpath, "//a[contains(@href,'auth/twitter')]").click
     fill_in 'email', with: 'email@example.com'
     click_button 'submit'
     expect(page).to have_content('Log out')
@@ -32,14 +34,14 @@ feature 'through twitter' do
     user.authorizations << auth
     mock_twitter_auth_hash
     visit new_user_session_path
-    click_link 'Sign in with Twitter'
+    page.find(:xpath, "//a[contains(@href,'auth/twitter')]").click
     expect(page).to have_content('Log out')
   end
 
   scenario 'can handle authentication error' do
     OmniAuth.config.mock_auth[:twitter] = :invalid_credentials
     visit new_user_session_path
-    click_link 'Sign in with Twitter'
+    page.find(:xpath, "//a[contains(@href,'auth/twitter')]").click
     expect(page).to have_content('Could not authenticate you from Twitter')
   end
 end
